72d9310ab137c79d6dcbd1360aef3add84b40ac1
KKWANH <kwanho0096@gmail.com>
2023-09-11 17:02:42 +0200

    #1 [repo] python env & opencv test

---
+++ app/python/setup/install.sh
@@ -0,0 +1,19 @@
+# variables setting
+export ADS_ENV_FOLDER="ads_venv"
+
+# python updating & install
+python3	-m pip install --upgrade pip
+python3	-m pip install --upgrade pip
+python3	-m pip install virtualenv
+
+# virtual env setting
+python3	-m virtualenv $ADS_ENV_FOLDER
+source  $ADS_ENV_FOLDER/bin/activate
+
+# pip version update
+pip3    install --upgrade pip
+
+# python libraries install
+pip3    install pandas \
+                numpy \
+                opencv-python
