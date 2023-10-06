2e4111f71764c97b5c77dba30b9e8adc967e2e0c
Shuta-syd <shuta.ogura0921@gmail.com>
2023-10-06 18:48:59 +0200

    #25 [fix] main.py loop error fix

--- src/main.py
+++ src/main.py
@@ -6,15 +6,26 @@ import piracer
 if __name__ == '__main__':
   vehicle = piracer.vehicles.PiRacerStandard()
   cap = cv2.VideoCapture(0)
-  ret, original_frame = cap.read()
+
+  cap.set(cv2.CAP_PROP_FPS, 20)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+  out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
+
   while(cap.isOpened()):
     ret, original_frame = cap.read()
     if ret == False:
       break
     frame, radian = lane.lane_tracker(original_frame)
+
+    out.write(frame)
     cv2.imshow('frame', frame)
     piracer.operate_radian.set_steering_radians(piracer=vehicle ,radians=radian)
     if cv2.waitKey(10) == 27:
       break
+
   cap.release()
+  out.release()
   cv2.destroyAllWindows()
