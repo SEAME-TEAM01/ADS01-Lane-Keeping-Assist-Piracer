8b18045d673a2bac0ddc52675a90551caf5203bf
Shuta-syd <shuta.ogura0921@gmail.com>
2023-10-06 18:33:44 +0200

    #25 [fix] module not found in main.py fixed

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 import cv2
 from lane_detection import lane
-from piracer.operate_radian import set_steering_radians
-from piracer.vehicles import PiRacerStandard
+import piracer
 
 
 if __name__ == '__main__':
-  vehicle = PiRacerStandard()
+  vehicle = piracer.vehicles.PiRacerStandard()
   cap = cv2.VideoCapture(0)
   ret, original_frame = cap.read()
   while(cap.isOpened()):
@@ -14,7 +13,7 @@ if __name__ == '__main__':
       break
     frame, radian = lane.lane_tracker(original_frame)
     cv2.imshow('frame', frame)
-    set_steering_radians(piracer=vehicle ,radians=radian)
+    piracer.operate_radian.set_steering_radians(piracer=vehicle ,radians=radian)
     if cv2.waitKey(10) == 27:
       break
   cap.release()
---
+++ src/piracer/__init__.py
@@ -0,0 +1,5 @@
+from . import operate_radian
+from . import operate_piracer
+from . import vehicles
+from . import gamepads
+from . import video_capture
