95c2bf7d8fe13f52b32e534ba7d248c6f99ad914
KKWANH <kwanho0096@gmail.com>
2023-10-06 14:24:52 +0100

    [dev] restructuring python app, moved venv into ~/envs/ads_venv, ...

--- app/python/setup.sh
+++ app/python/setup.sh
@@ -1,5 +1,6 @@
 # variables setting
 export ADS_ENV_FOLDER="ads_venv"
+mkdir ~/envs >> /dev/null
 
 # python updating & install
 python3	-m pip install --upgrade pip
@@ -7,14 +8,17 @@ python3	-m pip install --upgrade pip
 python3	-m pip install virtualenv
 
 # virtual env setting
-python3	-m virtualenv $ADS_ENV_FOLDER
-source  $ADS_ENV_FOLDER/bin/activate
+python3	-m virtualenv ~/envs/$ADS_ENV_FOLDER
+source  ~/envs/$ADS_ENV_FOLDER/bin/activate
 
 # pip version update
 pip3    install --upgrade pip
 
 # python libraries install
+pip3    install -r requirement.txt
 pip3    install pylint \
                 pandas \
                 numpy \
-                opencv-python
+                opencv-python \
+		picamera
+
