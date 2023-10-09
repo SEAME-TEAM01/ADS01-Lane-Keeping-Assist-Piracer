01423cb24b6ce892f38f6cac0167fa26440d73ca
Shuta-syd <shuta.ogura0921@gmail.com>
2023-10-09 18:31:59 +0200

    [update] operate_radian.py update

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import cv2
 from lane_detection.lane import Lane
+import math
 import piracer
 
 
 if __name__ == '__main__':
   vehicle = piracer.vehicles.PiRacerStandard()
   lane = Lane();
-  cap = cv2.VideoCapture('assets/piracer_pov.mp4')
+  cap = cv2.VideoCapture('piracer_pov.mp4')
 
   cap.set(cv2.CAP_PROP_FPS, 20)
   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -16,7 +17,7 @@ if __name__ == '__main__':
   # out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
 
   i = 0
-  while(i < 400):
+  while(i < 500):
     ret, original_frame = cap.read()
     if ret == False:
       break
@@ -24,7 +25,7 @@ if __name__ == '__main__':
 
     # out.write(frame)
     cv2.imshow('frame', frame)
-    # print(i, radian)
+
     piracer.operate_radian.set_steering_radians(piracer=vehicle ,radians=radian)
     if cv2.waitKey(10) == 27:
       break
--- src/piracer/operate_radian.py
+++ src/piracer/operate_radian.py
@@ -1,11 +1,17 @@
 import math
 
-def set_steering_radians(piracer=None, radians=1):
+def set_steering_radians(piracer=None, radian=1):
     if piracer is None:
         return
 
-    scaled_percent = radians / math.radians(45)
+    max_radian = math.radians(135)
+    min_radian = math.radians(45)
 
-    scaled_percent = max(min(scaled_percent, 1.0), -1.0)
+    if radian >= min_radian and radian <= max_radian:
+        scaled_percent = (radian - min_radian) / (max_radian - min_radian) * 2 - 1
+    else:
+        scaled_percent = -1.0 if radian < min_radian else 1.0
+
+    print(radian, scaled_percent)
 
     piracer.set_steering_percent(scaled_percent)
