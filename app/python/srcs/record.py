3fe218903712360691b389f4d2a9c55f7c86ba34
KKWANH <kwanho0096@gmail.com>
2023-10-10 16:41:25 +0100

    [dev] recording: multi process

--- app/python/srcs/record.py
+++ app/python/srcs/record.py
@@ -3,15 +3,42 @@ from        piracer.gamepads    import  ShanWanGamepad
 import      cv2                 as      cv2
 import      time                as      tim
 import      traceback           as      trc
+import      multiprocessing     as      mlp
 import      os
 
 CSVFILE     = "steering.csv"
 
-def car_control():
+def process_control_car():
     print("car control started.")
     pad = ShanWanGamepad()
     veh = PiRacerStandard()
 
+    try:
+        while True:
+            gamepad_input   = pad.read_data()
+            throttle        = gamepad_input.analog_stick_left.y
+            steering        = gamepad_input.analog_stick_right.y
+
+            veh.set_steering_percent(steering * 1.3)
+            veh.set_throttle_percent(throttle * 0.6)
+
+            # Log steering and frame info
+            with open(CSVFILE, "a") as csv_file:
+                csv_file.write(f"{tim.time()}, {steering}\n")
+
+    except Exception as e:
+        print("An error occurred:", e)
+        trc.print_exc()
+        veh.set_steering_percent(0)
+        veh.set_throttle_percent(0)
+
+    except KeyboardInterrupt:
+        print("Car control process has been stopped.")
+        veh.set_steering_percent(0)
+        veh.set_throttle_percent(0)
+
+def process_capture_img():
+    print("log data started.")
     # Start video capture
     cap = cv2.VideoCapture(0)
     
@@ -19,11 +46,9 @@ def car_control():
     if not cap.isOpened():
         print("Error: Could not open camera.")
         return
-
-    cnt = 0
+    
     try:
         while True:
-            # Capture frame-by-frame
             rst, frame = cap.read()
             frame = cv2.flip(frame, 0)
 
@@ -32,48 +57,26 @@ def car_control():
                 print("Error: Could not read frame.")
                 break
             
-            # Increment frame count
-            cnt += 1
-            
-            # Save the frame
-            cv2.imwrite(f'frame_{cnt}.jpg', frame)
-
-            gamepad_input   = pad.read_data()
-            throttle        = gamepad_input.analog_stick_right.y 
-            steering        = gamepad_input.analog_stick_left.x
-
-            veh.set_steering_percent(-steering)
-            veh.set_throttle_percent(throttle)
-
-            # Log steering and frame info
-            with open(CSVFILE, "a") as csv_file:
-                csv_file.write(f"{cnt}, {steering}\n")
-            
-            # Control the frequency of the loop - this will depend on the frame rate of your camera
-            # tim.sleep(0.1)
-
-    except Exception as e:
-        print(" - Car control process has been stopped. - ")
-        print("An error occurred:", e)     
-        trc.print_exc()
-
-        # Reset drivetrain
-        veh.set_steering_percent(0)
-        veh.set_throttle_percent(0)
-
-    except KeyboardInterrupt:
-        print("Car control process has been stopped.")
-        
-        veh.set_steering_percent(0)
-        veh.set_throttle_percent(0)
-
+            cv2.imwrite(f'frame_{tim.time()}.jpg', frame)
     finally:
-        # Release the video capture object
         cap.release()
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     if not os.path.exists(CSVFILE):
         with open(CSVFILE, "w") as file:
-            file.write("frame_count,steering\n")
-    car_control()
+            file.write("miliseconds,steering\n")
+
+    # pipe_car, pipe_img = mlp.Pipe()
+    pr1 = mlp.Process(
+        name="python3-car-control",
+        target=process_control_car,
+    )
+    pr2 = mlp.Process(
+        name="python3-capture-img",
+        target=process_capture_img,
+    )
+    pr1.start()
+    pr2.start()
+    pr1.join()
+    pr2.join()
